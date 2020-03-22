cee5c9bfc5e668da493e9ce3077e1692afee9bc2
unknown <harsharay.ray@gmail.com>
2020-03-22 17:58:16 +0530

    intial commit

---
+++ covidapp/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import requests
+from bs4 import BeautifulSoup
+
+# Create your views here.
+def home(request):
+    return render(request,'covidapp/home.html')
+
+def stats(request):
+    url = 'https://www.covidout.in'
+    page = requests.get(url)
+    soup_obj = BeautifulSoup(page.content,'html.parser')
+    output = soup_obj.findAll('h2',{'class':'case'})
+
+    numbers = []
+
+    for i in output:
+        numbers.append(i.text)
+
+    Confirmed_cases = numbers[0]
+    Hospitalized_cases = numbers[1]
+    Intensive_care_cases = numbers[2]
+    Recovered_cases = numbers[3]
+    Deaths = numbers[4]
+
+    return render(request,'covidapp/stats.html',{'Confirmed_cases':Confirmed_cases,'Hospitalized_cases':Hospitalized_cases,'Intensive_care_cases':Intensive_care_cases,'Recovered_cases':Recovered_cases,'Deaths':Deaths})
