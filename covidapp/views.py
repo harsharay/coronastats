9a5b9e924bb06b4e20ce5371cfad44a8eaee6002
unknown <harsharay.ray@gmail.com>
2020-03-23 23:42:23 +0530

    Cleaning/Removing unneccessary code

--- covidapp/views.py
+++ covidapp/views.py
@@ -12,26 +12,6 @@ def home(request):
     return render(request,'covidapp/home.html')
 
 def stats(request):
-    #url = 'https://www.covidout.in'
-    #page = requests.get(url)
-    #soup_obj = BeautifulSoup(page.content,'html.parser')
-    #output = soup_obj.findAll('h2',{'class':'case'})
-
-    #numbers = []
-
-    #for i in output:
-    #    numbers.append(i.text)
-
-    #Confirmed_cases = numbers[0]
-    #Hospitalized_cases = numbers[1]
-    #Intensive_care_cases = numbers[2]
-    #Recovered_cases = numbers[3]
-    #Deaths = numbers[4]
-
-    #for i in range(0,len(numbers)):
-    #   numbers[i] = int(numbers[i])
-
-    #return render(request,'covidapp/stats.html',{'Confirmed_cases':Confirmed_cases,'Hospitalized_cases':Hospitalized_cases,'Intensive_care_cases':Intensive_care_cases,'Recovered_cases':Recovered_cases,'Deaths':Deaths})
     return render(request, 'covidapp/stats.html')
 
 
